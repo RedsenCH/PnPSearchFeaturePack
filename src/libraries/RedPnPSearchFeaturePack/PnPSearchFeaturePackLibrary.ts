4a64471ac70c7dbad88d5a580211ec82a5b10ffb
Kevin LACEK <klacek@redsen.ch>
2023-10-06 17:04:28 +0200

    clean duplicate

--- src/libraries/RedPnPSearchFeaturePack/PnPSearchFeaturePackLibrary.ts
+++ src/libraries/RedPnPSearchFeaturePack/PnPSearchFeaturePackLibrary.ts
@@ -1,18 +1,19 @@
-import { IAdaptiveCardAction, 
-         IComponentDefinition, 
-         IDataSourceDefinition, 
-         IExtensibilityLibrary, 
-         ILayout, 
-         ILayoutDefinition, 
-         IQueryModifierDefinition, 
-         ISuggestionProviderDefinition, 
-         LayoutRenderType, 
-         LayoutType} from "@pnp/modern-search-extensibility";
+import {
+    IAdaptiveCardAction,
+    IComponentDefinition,
+    IDataSourceDefinition,
+    IExtensibilityLibrary,
+    ILayout,
+    ILayoutDefinition,
+    IQueryModifierDefinition,
+    ISuggestionProviderDefinition,
+    LayoutRenderType,
+    LayoutType,
+} from "@pnp/modern-search-extensibility";
 import LoggerService from "../../services/LoggerService/LoggerService";
 import { NewsCardsLayout } from "./CustomLayouts/NewsCards/NewsCardsLayout";
 import { FilterDateSliderWrapper } from "./CustomWebComponents/FilterDateSlider/FilterDateSliderWrapper";
 import { FilterNumericSliderWrapper } from "./CustomWebComponents/FilterNumericSlider/FilterNumericSliderWrapper";
-import { FilterYesNoWrapper } from "./CustomWebComponents/FilterYesNo/FilterYesNoWrapper";
 import { PageBookmarkWrapper } from "./CustomWebComponents/PageBookmark/PageBookmarkWrapper";
 import { PageCommentsWrapper } from "./CustomWebComponents/PageComments/PageCommentsWrapper";
 import { PageDateWrapper } from "./CustomWebComponents/PageDate/PageDateWrapper";
@@ -24,21 +25,24 @@ import { ServiceKey } from "@microsoft/sp-core-library";
 import { FilterYesNoCheckboxWebComponent } from "./CustomWebComponents/FilterYesNoCheckBox/FilterYesNoCheckBoxComponent";
 
 export class PnPSearchFeaturePackLibrary implements IExtensibilityLibrary {
-  getCustomLayouts(): ILayoutDefinition[] {
-    return [
-      {
-          name: 'News Cards Layout',
-          iconName: 'News',
-          key: 'REDNewsCardsLayout',
-          type: LayoutType.Results,
-          renderType: LayoutRenderType.Handlebars,
-          templateContent: require('./CustomLayouts/NewsCards/newscards-layout.html'),
-          serviceKey: ServiceKey.create<ILayout>('RED:NewsCardsLayout', NewsCardsLayout)
-      }
-  ];
-  }
-  getCustomWebComponents(): IComponentDefinition<any>[] {
-    return [
+    getCustomLayouts(): ILayoutDefinition[] {
+        return [
+            {
+                name: "News Cards Layout",
+                iconName: "News",
+                key: "REDNewsCardsLayout",
+                type: LayoutType.Results,
+                renderType: LayoutRenderType.Handlebars,
+                templateContent: require("./CustomLayouts/NewsCards/newscards-layout.html"),
+                serviceKey: ServiceKey.create<ILayout>(
+                    "RED:NewsCardsLayout",
+                    NewsCardsLayout
+                ),
+            },
+        ];
+    }
+    getCustomWebComponents(): IComponentDefinition<any>[] {
+        return [
             // BEGIN - Just for test
             // {
             //   componentName: 'mycustom-filtercombobox',
@@ -50,95 +54,101 @@ export class PnPSearchFeaturePackLibrary implements IExtensibilityLibrary {
             // },
             // END - Just for test
             {
-              componentName: 'filter-yesnocheckbox',
-              componentClass: FilterYesNoCheckboxWebComponent
+                componentName: "filter-yesnocheckbox",
+                componentClass: FilterYesNoCheckboxWebComponent,
             },
             {
-              componentName: 'filter-dateslider',
-              componentClass: FilterDateSliderWrapper
+                componentName: "filter-dateslider",
+                componentClass: FilterDateSliderWrapper,
             },
             {
-              componentName: 'filter-numericslider',
-              componentClass: FilterNumericSliderWrapper
+                componentName: "filter-numericslider",
+                componentClass: FilterNumericSliderWrapper,
             },
             {
-              componentName: 'filter-yesno',
-              componentClass: FilterYesNoWrapper
+                componentName: "filter-yesno",
+                componentClass: FilterYesNoWrapper,
             },
             {
-              componentName: 'panel-filepreview',
-              componentClass: PanelFilePreviewWrapper
+                componentName: "panel-filepreview",
+                componentClass: PanelFilePreviewWrapper,
             },
             {
-              componentName: 'page-bookmark',
-              componentClass: PageBookmarkWrapper
+                componentName: "page-bookmark",
+                componentClass: PageBookmarkWrapper,
             },
             {
-              componentName: 'page-comments',
-              componentClass: PageCommentsWrapper
+                componentName: "page-comments",
+                componentClass: PageCommentsWrapper,
             },
             {
-              componentName: 'page-like',
-              componentClass: PageLikeWrapper
+                componentName: "page-like",
+                componentClass: PageLikeWrapper,
             },
             {
-              componentName: 'page-date',
-              componentClass: PageDateWrapper
-            }];
-  }
+                componentName: "page-date",
+                componentClass: PageDateWrapper,
+            },
+        ];
+    }
 
-  /**
-   * 
-   * @returns 
-   */
-  getCustomSuggestionProviders(): ISuggestionProviderDefinition[] {
-    return [];
-  }
+    /**
+     *
+     * @returns
+     */
+    getCustomSuggestionProviders(): ISuggestionProviderDefinition[] {
+        return [];
+    }
 
-  /**
-   * 
-   * @param handlebarsNamespace 
-   */
-  registerHandlebarsCustomizations?(handlebarsNamespace: typeof Handlebars): void {
-    handlebarsNamespace.registerHelper('math', (lvalueStr:string, operator:string, rvalueStr:string) => {
-      const lvalue = parseFloat(lvalueStr);
-      const rvalue = parseFloat(rvalueStr);
-        
-      return {
-          "+": lvalue + rvalue,
-          "-": lvalue - rvalue,
-          "*": lvalue * rvalue,
-          "/": lvalue / rvalue,
-          "%": lvalue % rvalue
-      }[operator];
-    });
-  }
-  
-  /**
-   * 
-   * @param action 
-   */
-  invokeCardAction(action: IAdaptiveCardAction): void {
-    LoggerService.log("invokeCardAction - Not yet implemented");
-  }
-  
-  /**
-   * 
-   * @returns 
-   */
-  getCustomQueryModifiers?(): IQueryModifierDefinition[] {
-    return [];
-  }
+    /**
+     *
+     * @param handlebarsNamespace
+     */
+    registerHandlebarsCustomizations?(
+        handlebarsNamespace: typeof Handlebars
+    ): void {
+        handlebarsNamespace.registerHelper(
+            "math",
+            (lvalueStr: string, operator: string, rvalueStr: string) => {
+                const lvalue = parseFloat(lvalueStr);
+                const rvalue = parseFloat(rvalueStr);
 
-  /**
-   * 
-   * @returns 
-   */
-  getCustomDataSources?(): IDataSourceDefinition[] {
-    return [];
-  }
+                return {
+                    "+": lvalue + rvalue,
+                    "-": lvalue - rvalue,
+                    "*": lvalue * rvalue,
+                    "/": lvalue / rvalue,
+                    "%": lvalue % rvalue,
+                }[operator];
+            }
+        );
+    }
 
-  public name(): string {
-    return 'PnPSearchFeaturePackLibrary';
-  }
+    /**
+     *
+     * @param action
+     */
+    invokeCardAction(action: IAdaptiveCardAction): void {
+        LoggerService.log("invokeCardAction - Not yet implemented");
+    }
+
+    /**
+     *
+     * @returns
+     */
+    getCustomQueryModifiers?(): IQueryModifierDefinition[] {
+        return [];
+    }
+
+    /**
+     *
+     * @returns
+     */
+    getCustomDataSources?(): IDataSourceDefinition[] {
+        return [];
+    }
+
+    public name(): string {
+        return "PnPSearchFeaturePackLibrary";
+    }
 }
